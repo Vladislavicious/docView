643da8927aff2168c4a15509fbc8829019ed99af
vladislavicious <vladshilov03@mail.ru>
2024-10-20 15:53:28 +0300

    Сделал экран настроек

---
+++ app/src/main/java/com/example/pr2v6/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.pr2v6.ui.settings
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+}
